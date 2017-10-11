1b46cf3aec9cac83cb92efb5a8190c04b654cd6a
GertholdBoris2018 <boris.berthold2018@yandex.com>
2017-10-11 11:05:08 +0700

    Upload some files for js

---
+++ public/app/layout/shell.js
@@ -0,0 +1,11 @@
+﻿(function () { 
+    'use strict';
+    
+    var controllerId = 'shell';
+    angular.module('app').controller(controllerId,
+        ['$scope','$rootScope', 'common', 'config','$route','$location','$window', shell]);
+
+    function shell($scope,$rootScope, common, config ,route,$location,$window) {
+
+    };
+})();
